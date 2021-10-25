e19f54ca80318b4642e512b07f70e9deba61bbec
Thomas Drapeau <drapeau.th@gmail.com>
2021-10-25 20:19:56 +0200

    creating survey

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Home from './pages/Home'
+import Home from './pages/Home/index'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
